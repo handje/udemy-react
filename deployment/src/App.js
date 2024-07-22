39ebac95736bcedc032e013688381a2d5bd98088
Sonjieun <sonjieun2661@gmail.com>
2024-07-22 16:14:41 +0900

    build

--- deployment/src/App.js
+++ deployment/src/App.js
@@ -1,12 +1,13 @@
-//개발->테스팅->최적화->빌드->배포
+//개발->테스팅->최적화->빌드->배포(서버에 업로드)
 //최적화 : lazy loading
+//빌드 : npm run build(CRA일 경우)=> 프로덕션 빌드 => build폴더를 배포서버에 업로드
+//react는 spa라는 것을 인지하기, 정적웹서버호스트 사용하면 됨(static site host) ex)firebase host
+//spa로 설정해야 firebase 서버는 항상 index.html을 반환(클라이언트 라우팅 )
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
 import HomePage from "./pages/Home";
 import RootLayout from "./pages/Root";
-
-//blogpage를 필요할때만 로딩되도록(페이지와 로더 모두 적용)
 /*
 기존코드
 import BlogPage, { loader as postsLoader } from "./pages/Blog";
